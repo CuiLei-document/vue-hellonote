4bb735ecc74dc7779fb464edb1260af0e3754334
磊哥 <493114008@qq.com>
2021-10-06 12:43:06 +0800

    deploy

---
+++ precache-manifest.3bdb06560d0fd2df6994ab1005ab8b95.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1205f5cad0cf867507fd",
+    "url": "/vue-hellonote/css/app.1837134a.css"
+  },
+  {
+    "revision": "22b7cd13944765133a3b",
+    "url": "/vue-hellonote/css/chunk-2d732998.936d12b5.css"
+  },
+  {
+    "revision": "abacc62dbb26be7d7e8e",
+    "url": "/vue-hellonote/css/chunk-52dbdc47.8c981bc8.css"
+  },
+  {
+    "revision": "610a66de92e006cf2a4b",
+    "url": "/vue-hellonote/css/chunk-7c2c6694.7e046997.css"
+  },
+  {
+    "revision": "5c55f5a4bc7786df9f9e",
+    "url": "/vue-hellonote/css/chunk-vendors.1a9d9ed4.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-hellonote/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-hellonote/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d8ce13ae9c424dd86c023a29a90ee142",
+    "url": "/vue-hellonote/img/notebg.d8ce13ae.jpg"
+  },
+  {
+    "revision": "8e11f4dda95afc3bf2cf4a4442e14a9b",
+    "url": "/vue-hellonote/index.html"
+  },
+  {
+    "revision": "1205f5cad0cf867507fd",
+    "url": "/vue-hellonote/js/app.162990e4.js"
+  },
+  {
+    "revision": "22b7cd13944765133a3b",
+    "url": "/vue-hellonote/js/chunk-2d732998.6f1d064b.js"
+  },
+  {
+    "revision": "23687f898eb312c22d53",
+    "url": "/vue-hellonote/js/chunk-46e425d2.b65cb35d.js"
+  },
+  {
+    "revision": "abacc62dbb26be7d7e8e",
+    "url": "/vue-hellonote/js/chunk-52dbdc47.045c759a.js"
+  },
+  {
+    "revision": "610a66de92e006cf2a4b",
+    "url": "/vue-hellonote/js/chunk-7c2c6694.d7fb8485.js"
+  },
+  {
+    "revision": "5c55f5a4bc7786df9f9e",
+    "url": "/vue-hellonote/js/chunk-vendors.954d49c1.js"
+  },
+  {
+    "revision": "a679fe7d056abab46b6d4147cd455c06",
+    "url": "/vue-hellonote/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-hellonote/robots.txt"
+  }
+]);
